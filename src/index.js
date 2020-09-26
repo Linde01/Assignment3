104de3d20ef898871eae29fb70fe0a25b7e7b0b4
Linde01 <clinde331@gmail.com>
2020-09-26 15:33:10 +0800

    Javascript development restful api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 
 const app = express();
 app.use(express.json());
@@ -9,15 +8,63 @@ const Product = mongoose.model("Product", {
   name: { type: String, require: true },
   price: { type: Number, require: true },
 });
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const Category = mongoose.model("Category", {
+  name: { type: String, require: true },
+  description: { type: String, require: true },
+});
+
+app.get("/api/categories", async (req, res) => {
+  const categories = await Category.find();
+  res.send(categories);
+});
+
+app.get("/api/categories/:id", async (req, res) => {
+  const categories = await Category.findById(req.params.id);
+  res.send(categories);
+});
+
+app.post("/api/categories", async (req, res) => {
+  const newCategory = {
+    name: req.body.name,
+    description: req.body.description,
+  };
+  const category = new Category(newCategory);
+  const result = await category.save();
+  res.send(result);
+});
+
+app.put("/api/categories/:id", async (req, res) => {
+  const body = {
+    name: req.body.name,
+    description: req.body.description,
+  };
+  const category = await Category.findByIdAndUpdate(req.params.id, body, {
+    new: true,
+  });
+  res.send(category);
+});
+
+app.delete("/api/categories/:id", async (req, res) => {
+  const result = await Category.findByIdAndDelete(req.params.id);
+  res.send(result);
+});
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
 
 app.get("/api/products/:id", async (req, res) => {
   const products = await Product.findById(req.params.id);
   res.send(products);
 });
-app.post("api/products/", async (req, res) => {
-  const { name, price } = req.body;
-  const product = new Product({ name, price });
+app.post("/api/products", async (req, res) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const product = new Product(newProduct);
   const result = await product.save();
   res.send(result);
 });
@@ -47,6 +94,4 @@ const connectToDB = async () => {
   }
 };
 connectToDB();
-app.use(bodyParser.json());
-app.use(bodyParser.urlemcoded({ extended: true }));
 app.listen(3001, () => console.log(`Listening to Port 3001`));
